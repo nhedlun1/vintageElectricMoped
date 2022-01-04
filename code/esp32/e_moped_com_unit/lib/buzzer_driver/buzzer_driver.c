1e968a05b49cb1553e2e7ac697d247c3a1849be2
nhedlun1 <ledhunden@hotmail.com>
2022-01-04 14:28:14 +0100

    lots of changes, now task based

--- code/esp32/e_moped_com_unit/lib/buzzer_driver/buzzer_driver.c
+++ code/esp32/e_moped_com_unit/lib/buzzer_driver/buzzer_driver.c
@@ -3,12 +3,20 @@
 #include "driver/ledc.h"
 #include "common.h"
 
-#define LEDC_TIMER LEDC_TIMER_1
-#define LEDC_MODE LEDC_LOW_SPEED_MODE
-#define LEDC_CHANNEL LEDC_CHANNEL_1
-#define LEDC_DUTY_RES LEDC_TIMER_10_BIT // Set duty resolution to 13 bits
-#define LEDC_DUTY (512)                 // Set duty to 100%. ((2 ** 10) - 1) * 50% = 512
-#define LEDC_FREQUENCY (5000)           // Frequency in Hertz. Set frequency at 5 kHz
+#define BUZZER_TIMER LEDC_TIMER_1
+#define BUZZER_MODE LEDC_LOW_SPEED_MODE
+#define BUZZER_CHANNEL LEDC_CHANNEL_1
+#define BUZZER_DUTY_RES LEDC_TIMER_10_BIT // Set duty resolution to 13 bits
+#define BUZZER_DUTY (512)                 // Set duty to 100%. ((2 ** 10) - 1) * 50% = 512
+#define BUZZER_FREQUENCY (5000)           // Frequency in Hertz. Set frequency at 5 kHz
+#define PLAY_SONG_TASK_STACK_SIZE (1024)
+
+#define VOLUME_LOW 10
+#define VOLUME_MED 30
+#define VOLUME_HIGH 512
+
+static bool playing_tone = false;
+static uint8_t g_sound_to_play = 0;
 
 bool buzzer_init()
 {
@@ -17,17 +25,17 @@ bool buzzer_init()
 
     // Prepare and then apply the LEDC PWM timer configuration
     ledc_timer_config_t ledc_timer = {
-        .speed_mode = LEDC_MODE,
-        .timer_num = LEDC_TIMER,
-        .duty_resolution = LEDC_DUTY_RES,
-        .freq_hz = LEDC_FREQUENCY,
+        .speed_mode = BUZZER_MODE,
+        .timer_num = BUZZER_TIMER,
+        .duty_resolution = BUZZER_DUTY_RES,
+        .freq_hz = BUZZER_FREQUENCY,
         .clk_cfg = LEDC_AUTO_CLK};
 
     // Prepare and then apply the LEDC PWM channel configuration
     ledc_channel_config_t ledc_channel = {
-        .speed_mode = LEDC_MODE,
-        .channel = LEDC_CHANNEL,
-        .timer_sel = LEDC_TIMER,
+        .speed_mode = BUZZER_MODE,
+        .channel = BUZZER_CHANNEL,
+        .timer_sel = BUZZER_TIMER,
         .intr_type = LEDC_INTR_DISABLE,
         .gpio_num = BUZZER_PIN,
         .duty = 0, // Set duty to 0%
@@ -41,22 +49,79 @@ bool buzzer_init()
     return return_val;
 }
 
-bool buzzer_play_sound(uint8_t sound)
+void buzzer_play_sound_task(void *pvParameters)
 {
-    switch (sound)
+    switch (g_sound_to_play)
     {
-    case 1:
-        ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY);
-        ledc_set_freq(LEDC_MODE, LEDC_TIMER, NOTE_G6);
-        ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+    case TUNE_ACCESS_DENIED:
+        ledc_set_duty(BUZZER_MODE, BUZZER_CHANNEL, VOLUME_LOW);
+        ledc_update_duty(BUZZER_MODE, BUZZER_CHANNEL);
+        ledc_set_freq(BUZZER_MODE, BUZZER_TIMER, NOTE_G3);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        ledc_set_duty(BUZZER_MODE, BUZZER_CHANNEL, 0);
+        ledc_update_duty(BUZZER_MODE, BUZZER_CHANNEL);
+        break;
+    case TUNE_ACCESS_GRANTED:
+        ledc_set_duty(BUZZER_MODE, BUZZER_CHANNEL, VOLUME_LOW);
+        ledc_update_duty(BUZZER_MODE, BUZZER_CHANNEL);
+        ledc_set_freq(BUZZER_MODE, BUZZER_TIMER, NOTE_CS6);
+        vTaskDelay(250 / portTICK_PERIOD_MS);
+        ledc_set_freq(BUZZER_MODE, BUZZER_TIMER, NOTE_C6);
+        vTaskDelay(250 / portTICK_PERIOD_MS);
+        ledc_set_freq(BUZZER_MODE, BUZZER_TIMER, NOTE_DS6);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        ledc_set_duty(BUZZER_MODE, BUZZER_CHANNEL, 0);
+        ledc_update_duty(BUZZER_MODE, BUZZER_CHANNEL);
         break;
 
-    case 0:
+    case TUNE_WARNING:
+        ledc_set_duty(BUZZER_MODE, BUZZER_CHANNEL, VOLUME_HIGH);
+        ledc_update_duty(BUZZER_MODE, BUZZER_CHANNEL);
+        ledc_set_freq(BUZZER_MODE, BUZZER_TIMER, NOTE_C7);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        ledc_set_freq(BUZZER_MODE, BUZZER_TIMER, NOTE_C8);
+        ledc_update_duty(BUZZER_MODE, BUZZER_CHANNEL);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        ledc_set_duty(BUZZER_MODE, BUZZER_CHANNEL, 0);
+        ledc_update_duty(BUZZER_MODE, BUZZER_CHANNEL);
+        break;
+
+    case TUNE_HORN:
+        ledc_set_duty(BUZZER_MODE, BUZZER_CHANNEL, VOLUME_HIGH);
+        ledc_update_duty(BUZZER_MODE, BUZZER_CHANNEL);
+        ledc_set_freq(BUZZER_MODE, BUZZER_TIMER, NOTE_G3);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        ledc_set_duty(BUZZER_MODE, BUZZER_CHANNEL, 0);
+        ledc_update_duty(BUZZER_MODE, BUZZER_CHANNEL);
+        break;
+
+    case TUNE_LOCKING:
+        ledc_set_duty(BUZZER_MODE, BUZZER_CHANNEL, VOLUME_LOW);
+        ledc_update_duty(BUZZER_MODE, BUZZER_CHANNEL);
+        ledc_set_freq(BUZZER_MODE, BUZZER_TIMER, NOTE_B6);
+        vTaskDelay(250 / portTICK_PERIOD_MS);
+        ledc_set_freq(BUZZER_MODE, BUZZER_TIMER, NOTE_C6);
+        vTaskDelay(250 / portTICK_PERIOD_MS);
+        ledc_set_freq(BUZZER_MODE, BUZZER_TIMER, NOTE_GS5);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        ledc_set_duty(BUZZER_MODE, BUZZER_CHANNEL, 0);
+        ledc_update_duty(BUZZER_MODE, BUZZER_CHANNEL);
+        break;
+
     default:
-        ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 0);
-        ledc_set_freq(LEDC_MODE, LEDC_TIMER, LEDC_FREQUENCY);
-        ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
         break;
     }
-    return false;
+    vTaskDelete(NULL);
+}
+
+bool buzzer_play_sound(uint8_t sound)
+{
+    g_sound_to_play = sound;
+    bool return_val = false;
+    if (!playing_tone)
+    {
+        xTaskCreate(buzzer_play_sound_task, "buzzer_play_sound_task", PLAY_SONG_TASK_STACK_SIZE * 4, NULL, 10, NULL);
+        return_val = true;
+    }
+    return return_val;
 }
