2bce17a2caf6fff4f05f258b34dbc2e9d8e66a72
nhedlun1 <ledhunden@hotmail.com>
2022-01-04 21:54:32 +0100

    first commit, not finished

---
+++ code/esp32/e_moped_com_unit/lib/ma_filter/ma_filter.c
@@ -0,0 +1,90 @@
+#include "ma_filter.h"
+
+ma_filter_int_t *ma_init_int(uint8_t num_readings)
+{
+    ma_filter_int_t *new_ma_filter = NULL;
+    new_ma_filter = malloc(sizeof(ma_filter_int_t) + (num_readings * sizeof(int)));
+    if (new_ma_filter != NULL)
+    {
+        new_ma_filter->val_counter = 0;
+        new_ma_filter->num_readings = num_readings;
+        new_ma_filter->full = false;
+        for (int i = 0; i < new_ma_filter->num_readings; i++)
+        {
+            new_ma_filter->values[i] = 0;
+        }
+    }
+    return new_ma_filter;
+}
+
+ma_filter_float_t *ma_init_float(uint8_t num_readings)
+{
+    ma_filter_float_t *new_ma_filter = NULL;
+    new_ma_filter = malloc(sizeof(ma_filter_float_t) + (num_readings * sizeof(float)));
+    if (new_ma_filter != NULL)
+    {
+        new_ma_filter->val_counter = 0;
+        new_ma_filter->num_readings = num_readings;
+        new_ma_filter->full = false;
+        for (int i = 0; i < new_ma_filter->num_readings; i++)
+        {
+            new_ma_filter->values[i] = 0.0f;
+        }
+    }
+    return new_ma_filter;
+}
+
+int ma_get_avg_int(ma_filter_int_t *filter)
+{
+    float return_value = 0;
+    for (int i = 0; i < filter->num_readings; i++)
+    {
+        return_value += filter->values[i];
+        printf("value[%d]=%d\n", i, filter->values[i]);
+    }
+    int devider = (filter->full) ? filter->num_readings : filter->val_counter;
+    return (int)((return_value / devider) + 0.5f);
+}
+
+float ma_get_avg_float(ma_filter_float_t *filter)
+{
+    float return_value = 0;
+    for (int i = 0; i < filter->num_readings; i++)
+    {
+        return_value += filter->values[i];
+        printf("value[%d]=%f\n", i, filter->values[i]);
+    }
+
+    int devider = (filter->full) ? filter->num_readings : filter->val_counter;
+    return (return_value / devider);
+}
+
+void ma_add_value_int(ma_filter_int_t *filter, int value)
+{
+    if (filter->val_counter == filter->num_readings - 1)
+    {
+        filter->full = true;
+        filter->val_counter = 0;
+    }
+    filter->values[filter->val_counter++] = value;
+}
+
+void ma_add_value_float(ma_filter_float_t *filter, float value)
+{
+    if (filter->val_counter == filter->num_readings - 1)
+    {
+        filter->full = true;
+        filter->val_counter = 0;
+    }
+    filter->values[filter->val_counter++] = value;
+}
+
+void ma_free_filter_int(ma_filter_int_t *filter)
+{
+    free(filter);
+}
+
+void ma_free_filter_float(ma_filter_float_t *filter)
+{
+    free(filter);
+}
---
+++ code/esp32/e_moped_com_unit/lib/ma_filter/ma_filter.h
@@ -0,0 +1,56 @@
+#ifndef MA_FILTER_H
+#define MA_FILTER_H
+/**
+ * @file ma_filter.h
+ * @author Niklas Hedlund (nhedlund87@gmail.com)
+ * @brief Moving average module, enables user to add a value to an
+ * array and then read out a moving average, with or without being "mean average".
+ * @version 0.1
+ * @date 2022-01-04
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct
+{
+    uint8_t num_readings;
+    uint8_t val_counter;
+    bool full;
+    int *ma_value;
+    bool mean_average;
+    int values[]; //flexible struct member needs no size.
+} ma_filter_int_t;
+
+typedef struct
+{
+    uint8_t num_readings;
+    uint8_t val_counter;
+    bool full;
+    float *ma_value;
+    bool mean_average;
+    float values[]; //flexible struct member needs no size.
+} ma_filter_float_t;
+
+ma_filter_int_t *ma_init_int(uint8_t num_readings);
+
+ma_filter_float_t *ma_init_float(uint8_t num_readings);
+
+int ma_get_avg_int(ma_filter_int_t *filter);
+
+float ma_get_avg_float(ma_filter_float_t *filter);
+
+void ma_add_value_int(ma_filter_int_t *filter, int value);
+
+void ma_add_value_float(ma_filter_float_t *filter, float value);
+
+void ma_free_filter_int(ma_filter_int_t *filter);
+
+void ma_free_filter_float(ma_filter_float_t *filter);
+
+#endif /*MA_FILTER_H*/
