6faa41a17ad71182558dbfe5f90615d4cacc1e36
nhedlun1 <ledhunden@hotmail.com>
2022-01-12 15:23:00 +0100

    added comments

--- code/esp32/e_moped_com_unit/lib/can/can.h
+++ code/esp32/e_moped_com_unit/lib/can/can.h
@@ -4,10 +4,27 @@
 #include <stdbool.h>
 #include "common.h"
 
+/**
+ * @brief initialize the canbus.
+ * 
+ * @param data a pointer to the Vesc data struct that is used to store values read by can.
+ * @return true if initialized successfully
+ * @return false if it could not be initialized
+ */
 bool can_init(Vesc_data_t *data);
 
+/**
+ * @brief start/create the can task that listens to the canbus.
+ * 
+ * @return status_t STATUS_OK if initialized properly.
+ */
 status_t can_start_task();
 
+/**
+ * @brief getter for the can status.
+ * 
+ * @return status_t status of the can module.
+ */
 status_t can_get_status(void);
 
 #endif /*CAN_H*/
